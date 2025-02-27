76205da68d62bee0ee46884289abd29226e47b56
lil-ish <liliya070605@gmail.com>
2025-02-27 12:36:47 +0300

    Add files via upload

---
+++ js/contact.js
@@ -0,0 +1,16 @@
+
+document.getElementById('ticket-gift').addEventListener('click', function() {
+	window.location.href = 'https://widget.afisha.yandex.ru/w/sessions/NzIxODR8NTMxNjM3fDY4OTA2NzZ8MTcyNTgxODQwMDAwMA==?clientKey=819556cc-b959-48f8-80cf-4611a1792d85';
+});
+
+document.getElementById('ticket-old').addEventListener('click', function() {
+	window.location.href = 'https://widget.afisha.yandex.ru/w/sessions/NzIxODR8NTMxMDE3fDY4OTA2NzZ8MTczMjQxNzIwMDAwMA==?clientKey=819556cc-b959-48f8-80cf-4611a1792d85';
+});
+
+document.getElementById('ticket-soft').addEventListener('click', function() {
+	window.location.href = 'https://widget.afisha.yandex.ru/w/sessions/NzIxODR8NTMxMDE3fDY4OTA2NzZ8MTczMjQxNzIwMDAwMA==?clientKey=819556cc-b959-48f8-80cf-4611a1792d85';
+});
+
+document.getElementById('group').addEventListener('click', function() {
+	window.location.href = 'https://afisha.yandex.ru/moscow/excursions/gruppovaia-obzornaia-ekskursiia-po-vystavke-dali-pikasso';
+});
