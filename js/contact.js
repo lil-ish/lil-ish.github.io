6ed0ec8111b9b3d30157eea344eae5b796392ed2
lil-ish <liliya070605@gmail.com>
2025-02-28 18:35:10 +0300

    click contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,16 +1,24 @@
 
 document.getElementById('ticket-gift').addEventListener('click', function() {
-	window.location.href = 'https://widget.afisha.yandex.ru/w/sessions/NzIxODR8NTMxNjM3fDY4OTA2NzZ8MTcyNTgxODQwMDAwMA==?clientKey=819556cc-b959-48f8-80cf-4611a1792d85';
+	window.location.href = 'https://afisha.yandex.ru/moscow/excursions/gruppovaia-obzornaia-ekskursiia-po-vystavke-dali-pikasso';
 });
 
 document.getElementById('ticket-old').addEventListener('click', function() {
-	window.location.href = 'https://widget.afisha.yandex.ru/w/sessions/NzIxODR8NTMxMDE3fDY4OTA2NzZ8MTczMjQxNzIwMDAwMA==?clientKey=819556cc-b959-48f8-80cf-4611a1792d85';
+	window.location.href = 'https://afisha.yandex.ru/moscow/excursions/gruppovaia-obzornaia-ekskursiia-po-vystavke-dali-pikasso';
 });
 
 document.getElementById('ticket-soft').addEventListener('click', function() {
-	window.location.href = 'https://widget.afisha.yandex.ru/w/sessions/NzIxODR8NTMxMDE3fDY4OTA2NzZ8MTczMjQxNzIwMDAwMA==?clientKey=819556cc-b959-48f8-80cf-4611a1792d85';
+	window.location.href = 'https://afisha.yandex.ru/moscow/excursions/gruppovaia-obzornaia-ekskursiia-po-vystavke-dali-pikasso';
+});
+
+document.getElementById('ticket').addEventListener('click', function() {
+	window.location.href = 'https://afisha.yandex.ru/moscow/excursions/gruppovaia-obzornaia-ekskursiia-po-vystavke-dali-pikasso';
 });
 
 document.getElementById('group').addEventListener('click', function() {
+	window.location.href = 'https://afisha.yandex.ru/moscow/excursions/gruppovaia-obzornaia-ekskursiia-po-vystavke-dali-pikasso';
+});
+
+document.getElementById('group_1').addEventListener('click', function() {
 	window.location.href = 'https://afisha.yandex.ru/moscow/excursions/gruppovaia-obzornaia-ekskursiia-po-vystavke-dali-pikasso';
 });
